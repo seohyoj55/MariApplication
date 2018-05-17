4b759990967978870382e685914b00d4658725cd
seohyoj55 <seohyoj55@naver.com>
2018-05-17 16:38:39 +0900

    First Commit

---
+++ app/src/main/java/com/example/qasxe/mariapplication/Mari.java
@@ -0,0 +1,13 @@
+package com.example.qasxe.mariapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mari extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
